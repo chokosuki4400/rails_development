2c97f06853568be45c9e06817b11095d526107ed
chokosuki4400 <e.bird.onishi@gmail.com>
2018-09-11 02:05:49 +0900

    #5 ユーザー登録時のコントローラーを作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+
+  # 未ログイン時、ログインページへリダイレクト
+	# before_action :authenticate_user!
+	# before_filter :authenticate_person!
+
+  # CSRF保護をオンにする以下の1行を有効にします。
+  protect_from_forgery with: :exception
+  # ログイン済ユーザーのみにアクセスを許可する
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [
+      :userinfo_attributes => [:user_id, :name, :site_id, :profile, :image, :image_cache]
+    ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [
+      :userinfo_attributes => [:user_id, :name, :site_id, :profile, :image, :image_cache]
+    ])
+  end
+
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,9 +5,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def new
+    # super
+    # @user = User.new
+  end
 
   # POST /resource
   # def create
@@ -15,9 +16,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # GET /resource/edit
-  # def edit
-  #   super
-  # end
+  def edit
+    # super
+    @user = current_user
+  end
 
   # PUT /resource
   # def update
@@ -41,14 +43,20 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up) do |params|
+      params.permit(:email, :password, :password_confirmation, :current_password,
+                    userinfo_attributes: [:user_id, :name, :site_id, :profile, :image, :image_cache])
+    end
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update) do |params|
+      params.permit(:email, :password, :password_confirmation, :current_password,
+                    userinfo_attributes: [:user_id, :name, :site_id, :profile, :image, :image_cache])
+    end
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
