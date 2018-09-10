eed1a6c83795d17a82b87e5b554d28731d3b1b36
chokosuki4400 <e.bird.onishi@gmail.com>
2018-09-11 05:33:36 +0900

    #7 回答の関連付け

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,4 @@
 class Question < ApplicationRecord
   belongs_to :user, inverse_of: :question, optional: true
-  has_one :question, dependent: :destroy, inverse_of: :user
+  has_one :answers, dependent: :destroy, inverse_of: :user
 end
