776dd5dc357c9ea3753c7966ef8b1204bea8375c
chokosuki4400 <e.bird.onishi@gmail.com>
2018-09-12 16:32:51 +0900

    #7 レコードの修正

--- db/migrate/20180912063302_create_messages.rb
+++ db/migrate/20180912063302_create_messages.rb
@@ -2,8 +2,8 @@ class CreateMessages < ActiveRecord::Migration[5.2]
   def change
     create_table :messages do |t|
       t.references :user, null: false
-      t.text :message, null: false
-      t.text :answer
+      t.text :message_text, null: false
+      t.text :answer_text
       t.boolean :status, null: false, default: 0
 
       t.timestamps
