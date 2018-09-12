0be9377fadae919aee670da647eca97f432a0789
chokosuki4400 <e.bird.onishi@gmail.com>
2018-09-12 16:49:53 +0900

    #9 表示テンプレートを作成

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
     # resources :questions, only: [:index]
   end
 
-  # resources :entries
+  resources :messages
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
