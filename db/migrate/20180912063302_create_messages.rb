d3a3f38d3ddad8cc093e8ef162756868d03fa614
y-onishi4400 <e.bird.onishi@gmail.com>
2018-10-26 11:44:03 +0900

    fix:model_massage

--- db/migrate/20180912063302_create_messages.rb
+++ db/migrate/20180912063302_create_messages.rb
@@ -2,10 +2,10 @@ class CreateMessages < ActiveRecord::Migration[5.2]
   def change
     create_table :messages do |t|
       t.references :user, null: false
+      t.string :customer_ip
       t.text :message_text, null: false
       t.text :answer_text
       t.text :music_url
-      t.text :customer_ip
       t.boolean :twitter_flag, null: false, default: 0
       t.boolean :status, null: false, default: 0
 
