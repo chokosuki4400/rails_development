dc43b05f9a5925f47ed5b4aca6012a9a9ac81944
y-onishi4400 <e.bird.onishi@gmail.com>
2018-11-02 15:22:22 +0900

    feat:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,21 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+email = ["sato","suzuki","takahashi","tanaka"]
+names = ["佐藤", "鈴木", "高橋", "田中"]
+images = ["https://c.okmusic.jp/news_items/images/252544/more_large.jpg?1521096697","https://www.pakutaso.com/assets_c/2015/03/kawamurayuka-thumb-600x600-12130.jpg","https://www.pakutaso.com/assets_c/2017/02/dandadebu-thumb-600x600-30435.jpg","https://www.pakutaso.com/assets_c/2017/11/model_max_ezaki-thumb-800x800-37240.jpg"]
+
+0.upto(4) do |idx|
+  User.create!(
+    :email =>"#{email[idx]}@example.com",
+    :password => "password",
+    :encrypted_password => "password",
+    :name => "#{names[idx]}",
+    :nickname => "#{email[idx]}",
+    :profile => "よくある#{names[idx]}です。",
+    :image => "#{images[idx]}"
+  )
+end
