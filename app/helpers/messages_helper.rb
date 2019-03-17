78d1d3bcfcc3db1b7b4543e6eafeed27f06a0398
y-onishi4400 <e.bird.onishi@gmail.com>
2019-03-17 17:37:20 +0900

    fix:create send mail

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,21 +1,32 @@
 # frozen_string_literal: true
 
 module MessagesHelper
-
   require 'sendgrid-ruby'
   include SendGrid
 
   class << self
 
-    def send
+    def send(address, username, hash, url)
       from = SendGrid::Email.new(email: 'test@example.com')
-      to = SendGrid::Email.new(email: 'test@example.com')
-      subject = 'Sending with SendGrid is Fun'
-      content = SendGrid::Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
+      to = SendGrid::Email.new(email: address)
+      subject = username + 'さん、あなたに質問が届きました！'
+      hoge = address
+
+      # ヒアドキュメント内で変数を使う場合、
+      # #{x}で囲む
+      str = <<-EOS
+      あなたに質問が届きました！
+
+      質問を確認する：#{url}/#{hash}
+      EOS
+
+      content = SendGrid::Content.new(type: 'text/plain', value: str)
       mail = SendGrid::Mail.new(from, subject, to, content)
+
       sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
       response = sg.client.mail._('send').post(request_body: mail.to_json)
+      Rails.logger.debug(response.status_code)
     end
-  end
 
+  end
 end
